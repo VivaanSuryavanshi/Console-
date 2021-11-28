8c9df5432cdc5162203cd8d86998da0435014ed6
VivaanSuryavanshi <95156445+VivaanSuryavanshi@users.noreply.github.com>
2021-11-27 21:04:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log("Welcome to console screen!");
+
+var fname="Vivaan";
+//console.log(fname);
+
+var lname=" Suryavnshi";
+//console.log(lname);
+
+var fulname=fname.concat(lname);
+console.log(fulname);
+
+
+var replace=fulname.replace("Vivaan","Vivu");
+console.log(replace);
+
+console.log(fulname.toLowerCase());
+
+console.log(fulname.toUpperCase());
+
+console.log(Math.max(1,3,5,7));
+console.log(Math.min(1,3,5,7));
+
+
+
+var n1=30;
+var n2=10;
+var sub=n1-n2;
+console.log(sub);
